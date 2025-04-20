3049ef30e21567de72691c58694c43f5f83e4d5c
Team Lead <kwaisenglai@hotmail.com>
2025-04-20 12:10:28 +0800

    Commit 2

---
+++ PointOfSalesSystemWeb/js/hostname.js
@@ -0,0 +1,16 @@
+
+function printhost(){
+    var host = location.host;
+    document.getElementById("host").innerHTML = host
+}
+
+function printhostname(){
+    var hostname = location.hostname;
+    document.getElementById("hostname").innerHTML = hostname
+}
+
+`
+let hostname = location.host;
+document.getElementById("host").innerHTML = hostname
+
+`
